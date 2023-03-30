9d8a14b17e932021c2ff78888d2510687a4da6a7
alvaroLZ96 <alvarost54@gmail.com>
2023-03-30 20:36:47 +0200

    cors policy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 server.listen(5000, () => {
