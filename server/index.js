12927063c8334a06f639e10d67bbab7efe88b349
alvaroLZ96 <alvarost54@gmail.com>
2023-03-30 20:47:20 +0200

    cors again

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const cors = require("cors");
 
 server.use(cors());
 server.use(middlewares);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://for-three.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 server.use(router);
 server.listen(5000, () => {
   console.log("JSON Server is running");
