4f8ebd17b3f55f333f86022da799a4e036f151e8
alvaroLZ96 <alvarost54@gmail.com>
2023-03-30 20:31:52 +0200

    api changed

--- app/services/services.js
+++ app/services/services.js
@@ -1,10 +1,11 @@
 export const getData = async (param) => {
-    try {
-      const data = await fetch(`https://for-three-server.onrender.com/${param}`);
-      const dataJSON = await data.json();
-      return dataJSON;
-    } catch (error) {
-      console.log("Error:", error);
-    }
-  };
-  
+  try {
+    const data = await fetch(
+      `https://for-three-production.up.railway.app/${param}`
+    );
+    const dataJSON = await data.json();
+    return dataJSON;
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
